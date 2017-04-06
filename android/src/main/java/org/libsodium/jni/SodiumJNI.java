09d8f548466ecb2ac193bca2c0a19dcb35cefede
Scott Twiname <skott.twiname@gmail.com>
2017-04-06 16:16:52 +1200

    Android: add crypto_sign methods
    Added functions:
    - crypto_box_beforenm
    - crypto_sign_publickeybytes
    - crypto_sign_secretkeybytes
    - crypto_sign_seedbytes
    - crypto_sign_bytes
    - crypto_sign_detached
    - crypto_sign_verify_detached
    - crypto_sign_seed_keypair
    - crypto_sign_ed25519_sk_to_seed
    - crypto_sign_ed25519_pk_to_curve25519
    - crypto_sign_ed25519_sk_to_curve25519

--- android/src/main/java/org/libsodium/jni/SodiumJNI.java
+++ android/src/main/java/org/libsodium/jni/SodiumJNI.java
@@ -7,7 +7,7 @@ public class SodiumJNI {
   public final static native long randombytes_random();
   public final static native long randombytes_uniform(long upper_bound);
   public final static native void randombytes_buf(byte[] buf, int size);
-  public final static native int randombytes_close();
+  public final static native int  randombytes_close();
   public final static native void randombytes_stir();
 
   public final static native int crypto_secretbox_keybytes();
@@ -29,4 +29,16 @@ public class SodiumJNI {
   public final static native int crypto_box_keypair(byte[] pk, byte[] sk);
   public final static native int crypto_box_easy(byte[] c, final byte[] m, final long mlen, final byte[] n, final byte[] pk, final byte[] sk);
   public final static native int crypto_box_open_easy(byte[] m, final byte[] c, final long clen,  final byte[] n, final byte[] pk, final byte[] sk);
+  public final static native int crypto_box_beforenm(byte[] s, final byte[] pk, final byte[] sk);
+
+  public final static native int crypto_sign_publickeybytes();
+  public final static native int crypto_sign_secretkeybytes();
+  public final static native int crypto_sign_seedbytes();
+  public final static native int crypto_sign_bytes();
+  public final static native int crypto_sign_detached(byte[] sig, final byte[] msg, final int msg_len, final byte[] sk);
+  public final static native int crypto_sign_verify_detached(byte[] sig, byte[] msg, long msg_len, final byte[] pk);
+  public final static native int crypto_sign_seed_keypair(byte[] pk, byte[] sk, final byte[] seed);
+  public final static native int crypto_sign_ed25519_sk_to_seed(byte[] seed, final byte[] sk);
+  public final static native int crypto_sign_ed25519_pk_to_curve25519(byte[] curve25519_pk, final byte[] ed25519_pk);
+  public final static native int crypto_sign_ed25519_sk_to_curve25519(byte[] curve25519_sk, final byte[] ed25519_sk);
 }
